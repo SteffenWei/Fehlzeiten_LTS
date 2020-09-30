2030a2032a9557d231022a1b0be8c3a7c91822d9
E146U05 <E146U05@E146PC05.GYMLTS.BID.LOCAL>
2020-09-30 13:19:30 +0200

    Main update

--- lts/fehlzeiten_orga/main.java
+++ lts/fehlzeiten_orga/main.java
@@ -1,5 +1,63 @@
 package fehlzeiten_orga;
 
+
+
+
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+
+
+
+
 public class main {
- int aenderung;
+	
+	static List <Lehrer> Lehrerlist = new ArrayList<Lehrer>();
+	
+public static void main(String args[]){
+	
+	
+	
+	
+	
+}
+
+
+public static String ksuche(){ 
+
+	
+	
+	
+	
+	
+}
+private static void eingabeLuL(String tmpvname, String tmpnname, String tmpkrzl, int tmppersnr, int tmptelnr, String tmpmail, int  tmpfz){
+	
+	
+
+	Lehrer templehrer = new Lehrer(tmpvname, tmpnname, tmpkrzl, tmppersnr, tmptelnr, tmpmail, tmpfz);
+	
+	Lehrerlist.add(templehrer);
+	
+	
+	
+	
+}
+
+protected Lehrer suche(String tmpname){
+	
+	
+	return ;
+}
+
+private static void ausgabe(){
+	for(int i = 0; i < Lehrerlist.size(); i++){
+		
+		System.out.println(Lehrerlist.get(i).toString());
+	}
+}
+
 }
