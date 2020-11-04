7005b260ac388caa638dcac40927263f9482eb73
Schueler1LTS <Schueler1_LTS!>
2020-11-04 13:08:50 +0100

    seria

--- lts/fehlzeiten_orga/Lehrer.java
+++ lts/fehlzeiten_orga/Lehrer.java
@@ -10,7 +10,7 @@ public class Lehrer implements Serializable{
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+
 	private String nname;
 	private String vname;
 	private String krzl;
